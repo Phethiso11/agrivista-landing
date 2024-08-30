8951cd13eb73982d87370044ef34c7b096e9926d
Phethiso11 <phethisomasenyetse@gmail.com>
2024-08-30 16:04:35 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import writing from './icon.png';
+import web from './Group.png';
+import content from './Group (1).png'
+import social from './Icon (1).png'
+import insta from './Group 83.png'
+import dribble from './Group 84.png'
+import face from './Group 85.png'
+import be from './Group 86.png'
+import rate from './Rating.png'
+import pro from './Product.png'
+import sat from './Satisfied.png'
+import loc from './Team.png'
+import learn from './Learn More.png'
 function App() {
   return (
     <div className="App">
@@ -22,7 +34,7 @@ function App() {
       <section className="hero-section">
         <div className="hero-content">
           <h2>The Role of Technology in Revolutionizing Agriculture</h2>
-          <p>Learn how we are transforming the way we approach agriculture.</p>
+          <p>Li Europan lingues es membres del sam familie. Lor separate existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.</p>
           <div className="hero-buttons">
             <button className="btn">Get Started</button>
             <button className="btn learn-more">Learn More</button>
@@ -30,10 +42,10 @@ function App() {
         </div>
       </section>
 
-
+            
 
       <section className="passion-section">
-        <h2>Our Passion for Agriculture</h2>
+        <h2>Our Passion for Agriculture Nurturing Growth and Sustaining the Future</h2>
         <p>Nurturing Growth and Sustaining the Future</p>
         <div className="passion-content hero-buttons">
           <img src="https://s3-alpha-sig.figma.com/img/e762/1efc/add90d96b68fd53a1a45e58910474cf5?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=e897ZF7ppZpCW~FpTy1ga3dLMydKgFHK1DPpmtHJmhv50fvPcKqmMXdXD8gEI67STxDi9q9MtUWYVZSHKBviajEkdJ7RbLcirhhLISzc5mSXE3oANANWtuoBuf0RhFtOlXgrDWs3nbQub5cgS1R2feuLItB-OtC2WHhqb4ilRY-GtqbIdV-ROcsHfjPnzcjtUR~1J8SOyoTzI71gi1M2oK3VVMuFHmqjAyihtKxSyo5q261ZTrH0IUVY6cdUuq9GL-HtlJNBZD~BWYqSC-HS691QUWwwCiIAw1uWuAL98qqXMZpWtvICTVmJnPFuqEDiboeb1BCTvC-XtGIw9XIKuA__" alt="Agriculture" className="passion-image" />
@@ -46,19 +58,19 @@ function App() {
         <h2>Our Service</h2>
         <div className="services">
           <div className="service">
-            <i className="service-icon">📝</i>
+            <i className="service-icon"><img src={writing} alt=''></img></i>
             <p>Blog and Article Writing</p>
           </div>
           <div className="service">
-            <i className="service-icon">🌐</i>
+            <i className="service-icon"><img src={web} alt=''></img></i>
             <p>Website Content</p>
           </div>
           <div className="service">
-            <i className="service-icon">📊</i>
+            <i className="service-icon"><img src={content} alt=''></img></i>
             <p>Content Strategy and Consulting</p>
           </div>
           <div className="service">
-            <i className="service-icon">📱</i>
+            <i className="service-icon"><img src={social} alt=''></img></i>
             <p>Social Media Management</p>
           </div>
         </div>
@@ -89,35 +101,27 @@ function App() {
       <section className="sustainability-section">
         <h2>Sustainable Farming Meets Technology: Building a Greener Future</h2>
         <div className="sustainability-content hero-buttons">
-          <button className="btn learn-more">Learn More</button>
-          <button className="btn">Contact Us</button>
+          <button className="btn learn-more">Contact Us</button>
+          <button className="btn">Learn More</button>
         </div>
       </section>
 
       <section className="benefits-section">
-        <h2>The Benefits of Choosing Our Expertise</h2>
-        <div className="benefits">
-          <div className="benefit">
-            <i className="benefit-icon">⭐</i>
-            <p>15+ Years of Experience</p>
-          </div>
-          <div className="benefit">
-            <i className="benefit-icon">📦</i>
-            <p>40+ Products</p>
-          </div>
-          <div className="benefit">
-            <i className="benefit-icon">✅</i>
-            <p>2,458+ Satisfied Clients</p>
-          </div>
-          <div className="benefit">
-            <i className="benefit-icon">👥</i>
-            <p>20 Local Team Members</p>
-          </div>
+      <h2>The Benefits of Choosing Our Expertise</h2>
+      <div className="benefit">
+            <i className="benefit-icon"><img src={rate} alt=''></img></i>
+            <i className="benefit-icon"><img src={pro} alt=''></img></i>
+            <i className="benefit-icon"><img src={sat} alt=''></img></i>
+
+            <i className="benefit-icon"><img src={loc} alt=''></img></i>
+            
         </div>
+        <img src={learn} alt=''></img>
       </section>
 
       <section className="testimonials-section">
         <div className="testimonial">
+          <p>⭐⭐⭐⭐⭐</p>
           <img
             src="https://s3-alpha-sig.figma.com/img/eab9/67e2/46f834691e4bd80ec39ddeb67ea21df4?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mCSoECn5OXjRAqZcpHRMaTK7PqHttcq81bBxQwX6yqzQRqHo4BtMtFP-riRmUfSHpe5-Mc9bpr6GwgM79V6YSPnwk9kEKF7v-M6xDzO8vnlDXKKJ-YIhdORq7eCj0hWMXcp4lTPHnGi2Z~vys9eDQi18csvOUOvHlQkvRu7~Wp16KUgzdftQ~UOtOwCnvL~637yEQ2MJ0VK8BBoUQtd1vZN8mTZa2vrAr2I66QNpzlr2~FuU~e53qO66kwBBWpTKCju2OCQhmpdM0zDK68QNYbgzJUfM0ZhmhgL~vB-KJky4BfMAOeZ~HmPXFZFU8H-0krYd22QtcnEnCvFl4JiQ0w__" // Adjust the path based on where you store the image
             alt="Madeline Williamson"
@@ -140,19 +144,19 @@ function App() {
             <img src="https://s3-alpha-sig.figma.com/img/51e9/6456/382df2a4d4c1215dde86c1d074cf8306?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J1GEnCll0CeUg1yTKq5GnVHpfih0yRfoQPO3xLXPGDtCcy3eoZ15wIh3ZDLsHoJTgWkZDBVqQbCa7PJsX-HcwONcxppkPrEnjIfFZB4VUPr2K4Nis-p5M47UY-l5jlTBTj70wZVzEJhkeg8sZFZKc5rQ6lEbGBwKkD6-4x1ZQ-QQTVu7uvCSo~~iQHoTSWOyjs2uNKYtEEXDgC39QBRbZHU8rLYfxvLdd6DH2MPNXmZ8Eu1ciZfbku5O3Usme-KEmqIHLymhWdeADaRZTWplO9YxMABQFB8aQKaABcgSfY~oET8uRp5OOauRT3Kyvlg9uOiHzNldMgEhjRcrmwViGg__" alt="Blog 1" />
             <h3>Innovations for a Greener Future</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet...</p>
-            <a href="#">Read More</a>
+            <a href="#i">Read More</a>
           </div>
           <div className="blog-card">
             <img src="https://s3-alpha-sig.figma.com/img/2b4f/a100/8da35b5d8c4c53c319d1708152609689?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TELvBOFMG66IIcT1ZrivRHeEG40SMwoXY68b2lOymdOW6kJnP5GIFVIsffFCZZznximjHOfpH1TCq1BcclOveYhems29~5e8J3iKcMs-UpeVHiGQWZMdR5kHVbq4NFs~E9hYoao-V8X9Sl1HvYQTee9plRn-4FiQF20Rovu08fvtzDJ33Bya8Ah~~GcBuhgvZyo~j-oTX7YYVfDOyJA1HM9C0WHTVkaqKYEnf4gBT6hbwRIPyqBcRK9XEDwgOB1ma1ANmeVrUnWvUK0yBUinWmnWh2xF6lHnnzYDktMz609GoPRhd2k56Tgc2xCTDKlCwz852W4Ms8ewHmoJU36uEg__" alt="Blog 2" />
             <h3>The Power of Agricultural Analytics</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet...</p>
-            <a href="#">Read More</a>
+            <a href="#i">Read More</a>
           </div>
           <div className="blog-card">
             <img src="https://s3-alpha-sig.figma.com/img/c5bd/40ec/9cd491fd274939036f7cc0be57052e28?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JLlAFUFbKEeMa7H8wenj4ZuSpxV6xZ9MpXMBEkznc0EsO6DjJ6c~VO-joo86XQX-EAuOe2-sXbPWgvRUHBZ9LE11oUAjmo53yOzDaEkb1XcTJ96y5RYZ9pjFcBYCzJacxBZPkbSKvUv9slTTb9AwAPKV8YZDT8jdFJAlK6mywbnYMJYGuP3W~5cAXn6D6KpHQsfKk3qB0hwyQBmKcknDRms-r~uennVLhPhV64Z7x~15rjT~aB36-GRPcyrd9bm4nLZL1capW81DZNt4rqFtsM5jjebJVbgkjQ-SwCM0x7zrrjbE2giSmWr-Jg4cxmbyko8ORXTf~ATA2e4S8hWW4g__" alt="Blog 3" />
             <h3>Cultivating Sustainable and Resilient Farms</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet...</p>
-            <a href="#">Read More</a>
+            <a href="#i">Read More</a>
           </div>
         </div>
       </section>
@@ -163,10 +167,10 @@ function App() {
             <h2>AgriVista Farms</h2>
             <p>Li Europan lingues es membres del sam familie. Lor separate existentie.</p>
             <div className="footer-icons">
-              <a href="#i"><i className="icon-instagram"><img src='https://cdn-icons-png.flaticon.com/128/3991/3991634.png' alt=''></img></i></a>
-              <a href="#i"><i className="icon-dribbble"><img src='https://cdn-icons-png.flaticon.com/128/3488/3488315.png' alt=''></img></i></a>
-              <a href="#i"><i className="icon-facebook"><img src='https://cdn-icons-png.flaticon.com/128/1051/1051309.png' alt=''></img></i></a>
-              <a href="#i"><i className="icon-behance"><img src='https://cdn-icons-png.flaticon.com/128/2111/2111325.png' alt=''></img></i></a>
+              <a href="#i"><i className="icon-instagram"><img src={insta} alt=''></img></i></a>
+              <a href="#i"><i className="icon-dribbble"><img src={dribble} alt=''></img></i></a>
+              <a href="#i"><i className="icon-facebook"><img src={face} alt=''></img></i></a>
+              <a href="#i"><i className="icon-behance"><img src={be} alt=''></img></i></a>
             </div>
           </div>
           <div className="footer-section">
